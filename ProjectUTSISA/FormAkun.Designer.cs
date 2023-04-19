22ab5e8253291a270105046d7aa002e998449290
andrebayup14 <abayup1498@gmail.com>
2023-04-19 09:57:12 +0700

    Update FormAkun.Designer.cs

--- ProjectUTSISA/FormAkun.Designer.cs
+++ ProjectUTSISA/FormAkun.Designer.cs
@@ -30,6 +30,13 @@ namespace ProjectUTSISA
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.labelNIK = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.labelNoTelp = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.labelEmail = new System.Windows.Forms.Label();
@@ -41,13 +48,6 @@ namespace ProjectUTSISA
             this.labelnrk = new System.Windows.Forms.Label();
             this.labelnm = new System.Windows.Forms.Label();
             this.btnKembali = new System.Windows.Forms.Button();
-            this.labelNoTelp = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -56,7 +56,7 @@ namespace ProjectUTSISA
             // 
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Controls.Add(this.label7);
-            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.labelNIK);
             this.panel1.Controls.Add(this.label8);
             this.panel1.Controls.Add(this.label6);
             this.panel1.Controls.Add(this.labelNoTelp);
@@ -71,98 +71,161 @@ namespace ProjectUTSISA
             this.panel1.Controls.Add(this.labelNamaPengguna);
             this.panel1.Controls.Add(this.labelnrk);
             this.panel1.Controls.Add(this.labelnm);
-            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Location = new System.Drawing.Point(11, 10);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(541, 306);
+            this.panel1.Size = new System.Drawing.Size(481, 245);
             this.panel1.TabIndex = 6;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(286, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(167, 187);
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(152, 216);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(49, 16);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Alamat";
+            // 
+            // labelNIK
+            // 
+            this.labelNIK.AutoSize = true;
+            this.labelNIK.Location = new System.Drawing.Point(144, 10);
+            this.labelNIK.Name = "labelNIK";
+            this.labelNIK.Size = new System.Drawing.Size(28, 16);
+            this.labelNIK.TabIndex = 13;
+            this.labelNIK.Text = "NIK";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(11, 216);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(120, 16);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Alamat Pengguna: ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(29, 10);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(99, 16);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "NIK Pengguna: ";
+            // 
+            // labelNoTelp
+            // 
+            this.labelNoTelp.AutoSize = true;
+            this.labelNoTelp.Location = new System.Drawing.Point(149, 190);
+            this.labelNoTelp.Name = "labelNoTelp";
+            this.labelNoTelp.Size = new System.Drawing.Size(59, 16);
+            this.labelNoTelp.TabIndex = 11;
+            this.labelNoTelp.Text = "No. Telp";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 190);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(108, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Nomor Telepon: ";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(163, 86);
+            this.label1.Location = new System.Drawing.Point(145, 69);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(121, 20);
+            this.label1.Size = new System.Drawing.Size(100, 16);
             this.label1.TabIndex = 9;
             this.label1.Text = "Nama Lengkap";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(28, 86);
+            this.label3.Location = new System.Drawing.Point(25, 69);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(131, 20);
+            this.label3.Size = new System.Drawing.Size(106, 16);
             this.label3.TabIndex = 8;
             this.label3.Text = "Nama Lengkap: ";
             // 
             // labelEmail
             // 
             this.labelEmail.AutoSize = true;
-            this.labelEmail.Location = new System.Drawing.Point(167, 200);
+            this.labelEmail.Location = new System.Drawing.Point(148, 160);
             this.labelEmail.Name = "labelEmail";
-            this.labelEmail.Size = new System.Drawing.Size(51, 20);
+            this.labelEmail.Size = new System.Drawing.Size(41, 16);
             this.labelEmail.TabIndex = 7;
             this.labelEmail.Text = "Email";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(20, 200);
+            this.label2.Location = new System.Drawing.Point(18, 160);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(140, 20);
+            this.label2.Size = new System.Drawing.Size(112, 16);
             this.label2.TabIndex = 6;
             this.label2.Text = "Email Pengguna: ";
             // 
             // labelSaldo
             // 
             this.labelSaldo.AutoSize = true;
-            this.labelSaldo.Location = new System.Drawing.Point(165, 165);
+            this.labelSaldo.Location = new System.Drawing.Point(147, 132);
             this.labelSaldo.Name = "labelSaldo";
-            this.labelSaldo.Size = new System.Drawing.Size(51, 20);
+            this.labelSaldo.Size = new System.Drawing.Size(43, 16);
             this.labelSaldo.TabIndex = 5;
             this.labelSaldo.Text = "Saldo";
             // 
             // labelsld
             // 
             this.labelsld.AutoSize = true;
-            this.labelsld.Location = new System.Drawing.Point(24, 165);
+            this.labelsld.Location = new System.Drawing.Point(21, 132);
             this.labelsld.Name = "labelsld";
-            this.labelsld.Size = new System.Drawing.Size(135, 20);
+            this.labelsld.Size = new System.Drawing.Size(110, 16);
             this.labelsld.TabIndex = 4;
             this.labelsld.Text = "Saldo Rekening: ";
             // 
             // labelNoRek
             // 
             this.labelNoRek.AutoSize = true;
-            this.labelNoRek.Location = new System.Drawing.Point(165, 125);
+            this.labelNoRek.Location = new System.Drawing.Point(147, 100);
             this.labelNoRek.Name = "labelNoRek";
-            this.labelNoRek.Size = new System.Drawing.Size(59, 20);
+            this.labelNoRek.Size = new System.Drawing.Size(48, 16);
             this.labelNoRek.TabIndex = 3;
             this.labelNoRek.Text = "Nomor";
             // 
             // labelNamaPengguna
             // 
             this.labelNamaPengguna.AutoSize = true;
-            this.labelNamaPengguna.Location = new System.Drawing.Point(162, 48);
+            this.labelNamaPengguna.Location = new System.Drawing.Point(144, 38);
             this.labelNamaPengguna.Name = "labelNamaPengguna";
-            this.labelNamaPengguna.Size = new System.Drawing.Size(53, 20);
+            this.labelNamaPengguna.Size = new System.Drawing.Size(44, 16);
             this.labelNamaPengguna.TabIndex = 2;
             this.labelNamaPengguna.Text = "Nama";
             // 
             // labelnrk
             // 
             this.labelnrk.AutoSize = true;
-            this.labelnrk.Location = new System.Drawing.Point(17, 125);
+            this.labelnrk.Location = new System.Drawing.Point(15, 100);
             this.labelnrk.Name = "labelnrk";
-            this.labelnrk.Size = new System.Drawing.Size(138, 20);
+            this.labelnrk.Size = new System.Drawing.Size(112, 16);
             this.labelnrk.TabIndex = 1;
             this.labelnrk.Text = "Nomor Rekening:";
             // 
             // labelnm
             // 
             this.labelnm.AutoSize = true;
-            this.labelnm.Location = new System.Drawing.Point(17, 48);
+            this.labelnm.Location = new System.Drawing.Point(15, 38);
             this.labelnm.Name = "labelnm";
-            this.labelnm.Size = new System.Drawing.Size(142, 20);
+            this.labelnm.Size = new System.Drawing.Size(115, 16);
             this.labelnm.TabIndex = 0;
             this.labelnm.Text = "Nama Pengguna: ";
             // 
@@ -171,83 +234,22 @@ namespace ProjectUTSISA
             this.btnKembali.BackColor = System.Drawing.Color.Red;
             this.btnKembali.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnKembali.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnKembali.Location = new System.Drawing.Point(428, 332);
+            this.btnKembali.Location = new System.Drawing.Point(380, 266);
+            this.btnKembali.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnKembali.Name = "btnKembali";
-            this.btnKembali.Size = new System.Drawing.Size(114, 44);
+            this.btnKembali.Size = new System.Drawing.Size(101, 35);
             this.btnKembali.TabIndex = 9;
             this.btnKembali.Text = "Kembali";
             this.btnKembali.UseVisualStyleBackColor = false;
             // 
-            // labelNoTelp
-            // 
-            this.labelNoTelp.AutoSize = true;
-            this.labelNoTelp.Location = new System.Drawing.Point(168, 237);
-            this.labelNoTelp.Name = "labelNoTelp";
-            this.labelNoTelp.Size = new System.Drawing.Size(71, 20);
-            this.labelNoTelp.TabIndex = 11;
-            this.labelNoTelp.Text = "No. Telp";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(28, 237);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(133, 20);
-            this.label5.TabIndex = 10;
-            this.label5.Text = "Nomor Telepon: ";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(162, 12);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(36, 20);
-            this.label4.TabIndex = 13;
-            this.label4.Text = "NIK";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(33, 12);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(125, 20);
-            this.label6.TabIndex = 12;
-            this.label6.Text = "NIK Pengguna: ";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(171, 270);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(61, 20);
-            this.label7.TabIndex = 11;
-            this.label7.Text = "Alamat";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(12, 270);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(150, 20);
-            this.label8.TabIndex = 10;
-            this.label8.Text = "Alamat Pengguna: ";
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(322, 35);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(188, 234);
-            this.pictureBox1.TabIndex = 18;
-            this.pictureBox1.TabStop = false;
-            // 
             // FormAkun
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(565, 388);
+            this.ClientSize = new System.Drawing.Size(502, 310);
             this.Controls.Add(this.btnKembali);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FormAkun";
             this.Text = "Akun Pengguna";
             this.panel1.ResumeLayout(false);
@@ -274,7 +276,7 @@ namespace ProjectUTSISA
         private System.Windows.Forms.Label labelNoTelp;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelNIK;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.PictureBox pictureBox1;
