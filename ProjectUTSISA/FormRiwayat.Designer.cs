5bec8ccc1eb3bff8ed60129a19239e8b5ea38dde
Jennie03 <s160421039@student.ubaya.ac.id>
2023-05-10 12:00:24 +0700

    qawfesgrdthy
    srgdtfh

--- ProjectUTSISA/FormRiwayat.Designer.cs
+++ ProjectUTSISA/FormRiwayat.Designer.cs
@@ -31,7 +31,7 @@ namespace ProjectUTSISA
         {
             this.dataGridViewRiwayat = new System.Windows.Forms.DataGridView();
             this.btnKembali = new System.Windows.Forms.Button();
-            this.labelTitle = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewRiwayat)).BeginInit();
             this.SuspendLayout();
             // 
@@ -51,7 +51,7 @@ namespace ProjectUTSISA
             this.btnKembali.BackColor = System.Drawing.Color.Red;
             this.btnKembali.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnKembali.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnKembali.Location = new System.Drawing.Point(591, 389);
+            this.btnKembali.Location = new System.Drawing.Point(591, 382);
             this.btnKembali.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnKembali.Name = "btnKembali";
             this.btnKembali.Size = new System.Drawing.Size(101, 34);
@@ -60,24 +60,24 @@ namespace ProjectUTSISA
             this.btnKembali.UseVisualStyleBackColor = false;
             this.btnKembali.Click += new System.EventHandler(this.btnKembali_Click);
             // 
-            // labelTitle
+            // label1
             // 
-            this.labelTitle.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.labelTitle.AutoSize = true;
-            this.labelTitle.Font = new System.Drawing.Font("Tahoma", 20F, System.Drawing.FontStyle.Bold);
-            this.labelTitle.Location = new System.Drawing.Point(24, 21);
-            this.labelTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.labelTitle.Name = "labelTitle";
-            this.labelTitle.Size = new System.Drawing.Size(331, 41);
-            this.labelTitle.TabIndex = 48;
-            this.labelTitle.Text = "Riwayat Transaksi";
+            this.label1.BackColor = System.Drawing.Color.Navy;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(31, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(661, 55);
+            this.label1.TabIndex = 49;
+            this.label1.Text = "RIWAYAT TRANSAKSI";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // FormRiwayat
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(727, 443);
-            this.Controls.Add(this.labelTitle);
+            this.ClientSize = new System.Drawing.Size(727, 427);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnKembali);
             this.Controls.Add(this.dataGridViewRiwayat);
             this.Name = "FormRiwayat";
@@ -85,13 +85,12 @@ namespace ProjectUTSISA
             this.Load += new System.EventHandler(this.FormRiwayat_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewRiwayat)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.DataGridView dataGridViewRiwayat;
         private System.Windows.Forms.Button btnKembali;
-        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Label label1;
     }
 }
